66580a7b3836617fed72737c25b8f00b81af47c2
ReinerBalschun <reinermbalschun@gmail.com>
2024-06-26 14:41:34 +0200

    update header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React from 'react';
 
 function Header(){
     return(
-        <header className="border-b">
+        <header className="border-b font-bold p-3">
             AppName
         </header>
     )
