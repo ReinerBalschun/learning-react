05e1b63fb7eb99de82d8c963a02e1fefd2bddb5c
ReinerBalschun <reinermbalschun@gmail.com>
2024-06-26 14:27:26 +0200

    Chapter 2 learning "react"

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './public/index.html',
+    '.src/**/*.{html,js}',
+    './src/**/*.js'
+    
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
